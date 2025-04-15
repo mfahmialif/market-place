372f8ef88a78290c6e19205897999a0df93a8867
mfahmialif <mfahmialif@gmail.com>
2025-04-15 22:40:14 +0700

    update landing

---
+++ landing-page/assets/dist/js/gallery-slide.js
@@ -0,0 +1,11 @@
+const gallerySwiper = new Swiper(".mySwiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
